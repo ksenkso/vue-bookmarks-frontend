719730750a31d4a5498966c5d95d38b7141c0ea5
ksenkso <yazunya@gmail.com>
2018-08-01 01:10:36 +0300

    Update bookmark styles and markup. Now bookmark can be deleted.

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="bookmark">
-    <a :href="bookmark.url" class="bookmark__link">
+  <div ref="el" class="bookmark" :class="bookmarkClass">
+    <div class="bookmark__background" :class="backgroundClass" :style="{backgroundColor: bookmark.background}">
+        <img
+          class="bookmark__image"
+          :src="bookmark.img | apiUrl"
+          :alt="bookmark.title"
+        >
+      </div>
+    <a target="_blank" :href="bookmark.url" class="bookmark__link">
       <h1 class="bookmark__title">{{bookmark.title}}</h1>
-      <span class="bookmark__background" :class="backgroundClass" :style="{backgroundColor: bookmark.background}">
-            <img
-              class="bookmark__image"
-              :src="bookmark.img | apiUrl"
-              :alt="bookmark.title"
-            >
-          </span>
+      <span class="bookmark__url">{{bookmark.url}}</span>
     </a>
-    <div class="bookmark__updated">Upd: {{bookmark.updatedAt|date}}</div>
     <div class="bookmark__buttons">
-      <button @click="editBookmark" type="button" class="btn btn-sm btn-primary">Edit</button>
-      <button @click="deleteBookmark" type="button" class="btn btn-sm btn-danger">Delete</button>
+      <button @click="editBookmark" type="button" class="bookmark__edit icon icon-edit">Edit</button>
+      <button @click="deleteBookmark" type="button" class="bookmark__delete icon icon-delete">Delete</button>
     </div>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'Bookmark',
     props: ['bookmark'],
@@ -26,6 +27,18 @@
     computed: {
       backgroundClass() {
         return this.bookmark.imageType === 'ICO' ? 'bookmark__background_ico' : 'bookmark__background_splash';
+      },
+      bookmarkClass() {
+        let className = [];
+        if (this.isDisabled) className.push('bookmark_disabled');
+        if (this.isHidden) className.push('bookmark_hidden');
+        return className.join(' ').trim();
+      }
+    },
+    data() {
+      return {
+        isDisabled: false,
+        isHidden: false
       }
     },
     methods: {
@@ -33,33 +46,119 @@
 
       },
       deleteBookmark() {
-
-      }
+        const shouldDelete = confirm('Do you really want to delete this bookmark?');
+        if (shouldDelete) {
+          this.isDisabled = true;
+          this.apiClient.deleteBookmark(this.bookmark.id)
+            .then(this.remove)
+            .then(() => {
+              this.$emit('deleted')
+            })
+        }
+      },
+      remove() {
+        this.isDisabled = true;
+        return new Promise((resolve) => {
+          const rect = this.$refs.el.getBoundingClientRect();
+          const resultWidth = rect.right + rect.width;
+          console.log(resultWidth);
+          const tl = new TimelineLite();
+          tl
+            .to(this.$refs.el, .5, {
+              left: resultWidth,
+              ease: Power2.easeInOut
+            })
+            .to(this.$refs.el, .5, {
+              height: 0,
+              ease: Power2.easeInOut,
+              onComplete: resolve
+            }, 0.2)
+          /*const tl = TweenLite
+            .to(this.$refs.el, .2, {
+              left: resultWidth,
+              ease: Power0.easeInOut,
+            });*/
+          // setTimeout(resolve, 40000);
+        });
+      },
     }
   };
 </script>
 
 <style scoped lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Fira+Sans|PT+Sans:700&subset=cyrillic')
+@keyframes fade-in
+  0%
+    opacity: 0
+  100%
+    opacity: 1
 .bookmark
+  position: relative
   max-width: 512px
   height: 268px
   margin: 0 auto 16px
-  border: 1px solid #efefef
   border-radius: 3px
   overflow: hidden
+  /*transition: height .2s ease-in-out, margin .2s ease-in-out*/
+  box-shadow: 0 0 6.09px 0.91px rgba(0, 0, 0, 0.13)
   /* Fix this*/
-  font-family: 'Helvetica Neue', 'Helvetica', Tahoma, Arial, sans-serif
-  &__link
-    text-decoration: none
+  font-family: 'Fira Sans', sans-serif
+  &_hidden
+    height: 0
+    margin-bottom: 0
+  &_disabled
+    &:after
+      content: ''
+      display: block
+      width: 100%
+      height: 100%
+      position: absolute
+      top: 0
+      left: 0
+      background-color: rgba(255, 255, 255, .7)
+      animation: fade-in .1s forwards
+  &__url
     color: #fff
-    position: relative
-    display: block
+    font-size: 18px
+    margin-bottom: 4px
+    padding: 4px 12px
+    background-color: rgba(44, 44, 44, 0.5)
+    border-radius: 15px
+    text-align: center
+  &__link
+    display: flex
+    flex-direction: column
+    justify-content: space-between
     height: 100%
     padding: 16px
+    text-decoration: none
+    color: #fff
   &__title
     font-size: 2em
-    margin-bottom: 1rem
+    margin-top: 14px
     font-weight: bold
+    font-family: 'PT Sans', sans-serif
+  &__buttons
+    position: absolute
+    top: 38px
+    right: 23px
+    width: 80px
+    display: flex
+    justify-content: space-between
+    button
+      background-color: rgba(0, 0, 0, 0)
+      background-repeat: no-repeat
+      background-size: 24px
+      background-position: center
+      font-size: 0
+      width: 32px
+      height: 32px
+      border-radius: 50%
+      /* Fix this to use proper animation (Google-like) */
+      transition: background-color .1s ease-in-out
+      &:active
+        background-color: rgba(0, 0, 0, .3)
+
   &__background
     width: 100%
     height: 100%
@@ -71,7 +170,15 @@
     right: 0
     bottom: 0
     z-index: -1
-    background-image: linear-gradient(rgba(0, 0, 0, .45), rgba(0, 0, 0, .45))
+    &:after
+      content: ''
+      display: block
+      position: absolute
+      background-image: linear-gradient(rgba(0, 0, 0, .45), rgba(0, 0, 0, .45))
+      width: 100%
+      height: 100%
+      top: 0
+      left: 0
     &_splash
       .bookmark__image
         width: 100%
@@ -88,7 +195,4 @@
         position: absolute
   &__image
     display: block
-
-
-
 </style>
