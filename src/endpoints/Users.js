e3469ec6b07a9f87c026adca15e7101d61d881d3
ksenkso <yazunya@gmail.com>
2018-07-31 11:33:35 +0300

    API routes objects for users and bookmarks

---
+++ src/endpoints/Users.js
@@ -0,0 +1,14 @@
+export default {
+  me: {
+    url: '/users/me',
+    defaults: {
+      method: 'GET'
+    }
+  },
+  login: {
+    url: '/users/login',
+    defaults: {
+      method: 'POST'
+    }
+  }
+};
