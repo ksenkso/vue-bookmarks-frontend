c579e360098a091272eaed5304c3777bcd7342bc
ksenkso <yazunya@gmail.com>
2018-08-05 13:48:49 +0300

    Common header component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+
+    </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  };
+</script>
+
+<style scoped lang="sass">
+.header
+  height: 80px
+  background-color: #2874b4
+</style>
