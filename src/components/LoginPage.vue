3206501a415e7ca4cdbff8c4e99f9ee25db06188
ksenkso <yazunya@gmail.com>
2018-08-05 13:44:48 +0300

    Refactor login page to use actions

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -3,11 +3,11 @@
     <form @submit.prevent="logIn" action="#" class="login__form form">
       <div class="form__field">
         <label class="form__label" for="inp_email">Email</label>
-        <input type="email" name="email" id="inp_email" class="form__input" placeholder="email@domain.com" required>
+        <input v-model="email" type="email" name="email" id="inp_email" class="form__input" placeholder="email@domain.com" required>
       </div>
       <div class="form__field">
         <label class="form__label" for="inp_password">Password</label>
-        <input type="password" name="password" id="inp_password" class="form__input" placeholder="Your password"
+        <input v-model="password" type="password" name="password" id="inp_password" class="form__input" placeholder="Your password"
                required>
       </div>
       <div class="form__buttons">
@@ -15,47 +15,78 @@
         <div id="signIn"></div>
       </div>
     </form>
-    <ul class="errors"></ul>
+    <ul class="errors" v-if="errors && errors.length">
+      <li class="error error_red" v-for="error in errors">{{error.message}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-
+  import Vue from 'vue';
+  import {mapGetters} from 'vuex';
   export default {
     name: 'LoginPage',
     inject: ['apiClient'],
     methods: {
-      logIn() {
-
+      async logIn() {
+        try {
+          const {email, password} = this;
+          await this.$store.dispatch('login', {email, password});
+          console.log(this.$store.getters.user);
+        } catch (e) {
+            console.error(e)
+        }
       },
       async loginWithGoogle(googleUser) {
         const id_token = googleUser.getAuthResponse().id_token;
         // Clear the errors list
         //errors.innerHTML = '';
         try {
-          await this.apiClient.login(id_token);
+          const user = await this.$store.dispatch('login', {token: id_token});
+          console.log(user);
           this.$router.push('/');
         } catch (e) {
           console.error(e);
         }
       },
       handleGoogleLoginFail(err) {
-
+        console.error(err);
       }
     },
     created() {
       this.$nextTick(() => {
-        this.$renderSignInButton({
+        Vue.$renderSignInButton({
           id: 'signIn',
           onsuccess: this.loginWithGoogle.bind(this),
           onerror: this.handleGoogleLoginFail.bind(this)
         });
       })
+    },
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    computed: {
+      ...mapGetters(['errors'])
     }
   };
 </script>
 
 <style lang="sass">
+  .errors
+    max-width: 466px
+    margin: 24px auto 0
+    .error
+      padding: 8px
+      border-radius: 3px
+      margin-bottom: 8px
+      &:last-child
+        margin-bottom: 0
+      &_red
+        background-color: #f8d7da
+        color: #000
   .login
     margin-top: 180px
     .form
